519d6aedcf2e0c42f35ae0145fed7f616cd06e2d
ermolenjke <ermolenko.daniil.al@gmail.com>
2022-06-28 22:18:50 +0300

    first controller

---
+++ aliasGame/Extension/UIkit/UIColor + Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor + Extensions.swift
+//  aliasGame
+//
+//  Created by Даниил Ермоленко on 28.06.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let specialRed = #colorLiteral(red: 0.9529411765, green: 0.5058823529, blue: 0.5058823529, alpha: 1)
+    static let specialYellow = #colorLiteral(red: 0.9882352941, green: 0.8901960784, blue: 0.5411764706, alpha: 1)
+    static let specialOlives = #colorLiteral(red: 0.9176470588, green: 1, blue: 0.8156862745, alpha: 1)
+    static let specialCian = #colorLiteral(red: 0.5843137255, green: 0.8823529412, blue: 0.8274509804, alpha: 1)
+
+}
--- aliasGame/MainViewController.swift
+++ aliasGame/MainViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    private let aliasLogo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "aliasLogo")
+        imageView.backgroundColor = .specialOlives
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .magenta
+        view.backgroundColor = .specialOlives
+        setupView()
+        setupConstrains()
     }
-
-
+    
+    private func setupView() {
+        view.addSubview(aliasLogo)
+    }
+    
+    private func setupConstrains() {
+        
+        NSLayoutConstraint.activate([
+            
+            aliasLogo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            aliasLogo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            aliasLogo.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            aliasLogo.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            
+        ])
+    }
+    
 }
 
